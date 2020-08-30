161535cde35721ee0d59044b5c63c95961646647
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2020-08-30 13:13:50 +0200

    Enhance White

--- AmbientDisplay/transmit/transmitter.cpp
+++ AmbientDisplay/transmit/transmitter.cpp
@@ -8,7 +8,9 @@
 
 #include "transmitter.hpp"
 #include <array>
+#include <cmath>
 #include <iostream>
+#include <algorithm>
 #include <chrono>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -33,6 +35,45 @@ Pixel toPixel(unsigned int value){
     pixel.green = uchar((value >> 8) & 0xFF);
     pixel.blue = uchar(value & 0xFF);
     return pixel;
+}
+
+Pixel enhanceWhite(Pixel input){
+    auto Ri = input.red;
+    auto Gi = input.green;
+    auto Bi = input.blue;
+    //Get the maximum between R, G, and B
+    float tM = std::max(Ri, std::max(Gi, Bi));
+
+    //If the maximum value is 0, immediately return pure black.
+    if(tM == 0) return Pixel{};
+
+    //This section serves to figure out what the color with 100% hue is
+    float multiplier = 255.0f / tM;
+    float hR = Ri * multiplier;
+    float hG = Gi * multiplier;
+    float hB = Bi * multiplier;
+
+    //This calculates the Whiteness (not strictly speaking Luminance) of the color
+    float M = std::max(hR, std::max(hG, hB));
+    float m = std::min(hR, std::min(hG, hB));
+    float Luminance = ((M + m) / 2.0f - 127.5f) * (255.0f/127.5f) / multiplier;
+
+    //Calculate the output values
+    uchar Wo = uchar(lround(Luminance));
+    uchar Bo = uchar(lround(Bi - Luminance));
+    uchar Ro = uchar(lround(Ri - Luminance));
+    uchar Go = uchar(lround(Gi - Luminance));
+
+    //Trim them so that they are all between 0 and 255
+    if (Wo < 0) Wo = 0;
+    if (Bo < 0) Bo = 0;
+    if (Ro < 0) Ro = 0;
+    if (Go < 0) Go = 0;
+    if (Wo > 255) Wo = 255;
+    if (Bo > 255) Bo = 255;
+    if (Ro > 255) Ro = 255;
+    if (Go > 255) Go = 255;
+    return Pixel{Ro, Go, Bo, Wo};
 }
 
 std::array<int, 24> pixel_to_dmx = {
@@ -93,8 +134,6 @@ void Transmitter::run() {
         auto result = send(socketfd, &packet, sizeof(ArtNetDmxPacket), 0);
         if (result<0){
             std::cout << "Error: " << errno << std::endl;
-        } else {
-            std::cout << "Success" << std::endl;
         }
     }
 }
@@ -103,9 +142,9 @@ std::vector<Pixel> Transmitter::handle_record(RecordPtr record) {
     int pixel_count = 24;
     std::vector<Pixel> pixels;
     for (int i = 0;i<pixel_count;i++){
-        int offset_x = double(record->width())*double(i)/double(pixel_count);
-        auto pixel = toPixel(record->pixel(offset_x,30));
-        pixels.push_back(pixel);
+        int offset_x = int(lround(double(record->width())*double(i)/double(pixel_count)));
+        auto pixel = toPixel(record->pixel(offset_x,29));
+        pixels.push_back(enhanceWhite(pixel));
     }
     return pixels;
 }
